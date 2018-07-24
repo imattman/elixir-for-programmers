cfae0b35cd2fe6f2e672d2165f690a18c6592ce7
Matt Phillips <mattman@mattcave.com>
2018-07-23 21:52:27 -0700

    Additional refactoring and cleanup of hangman core logic
    - moved notion of 'tally' from lower Game up to Hangman (API) level
    - fleshed out defdelegate calls

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -6,6 +6,12 @@ defmodule Hangman do
 #    Game.new_game()
 #  end
 
-defdelegate new_game(), to: Game
+  defdelegate new_game(),             to: Game
+  defdelegate tally(game),            to: Game
 
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    { game, tally(game) }
+  end
+  
 end
