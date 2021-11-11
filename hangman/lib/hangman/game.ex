2c2e8c3bb67bc341e6447e5eeb6e065dc61490f7
Matt Phillips <matt.phillips@gmail.com>
2021-11-10 16:48:02 -0800

    wip: added tests for :won and :lost games

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,18 +1,30 @@
 defmodule Hangman.Game do
-
   # one struct allowed per module
   # it's name is the same as the module
   defstruct(
-      turns_left: 7,
-      game_state: :initializing,
-      letters: [],
-
+    turns_left: 7,
+    game_state: :initializing,
+    letters: []
   )
 
   def new_game() do
     %Hangman.Game{
-      letters: Dictionary.random_word |> String.codepoints
-      }
+      letters: Dictionary.random_word() |> String.codepoints()
+    }
   end
 
+  def make_move(game = %{game_state: :won}, _guess) do
+    {game, tally(game)}
+  end
+
+  def make_move(game = %{game_state: :lost}, _guess) do
+    {game, tally(game)}
+  end
+
+  # def make_move(game, guess) do
+  # end
+
+  def tally(_game) do
+    123
+  end
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -10,5 +10,23 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
 
+    # verify all letters fall in expected range
+    assert game.letters |> Enum.all?(fn c -> c =~ ~r/[a-z]/ end)
+  end
+
+  test "game state isn't changed for :won game" do
+    game = Game.new_game() |> Map.put(:game_state, :won)
+
+    # one way
+    # {new_game, _} = Game.make_move(game, "x")
+    # assert new_game == game
+
+    # other way using pinned match
+    assert {^game, _} = Game.make_move(game, "x")
+  end
+
+  test "game state isn't changed for :lost game" do
+    game = Game.new_game() |> Map.put(:game_state, :lost)
+    assert {^game, _} = Game.make_move(game, "x")
   end
 end
