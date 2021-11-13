b5acec757a1d1438e592f340f81618e56fd81606
Matt Phillips <matt.phillips@gmail.com>
2021-11-13 15:59:01 -0800

    wip: initial text client subproject (pre refactor of state)

---
+++ text_client/lib/text_client/interact.ex
@@ -0,0 +1,27 @@
+defmodule TextClient.Interact do
+  defstruct(
+    game_service: nil,
+    tally: nil,
+    guessed: ""
+  )
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+  end
+
+  def play(state) do
+    # state = interact(state)
+    # state = interact(state)
+    # state = interact(state)
+
+    play(state)
+  end
+
+  defp setup_state(game) do
+    %TextClient.Interact{
+      game_service: game,
+      tally: Hangman.tally(game)
+    }
+  end
+end
