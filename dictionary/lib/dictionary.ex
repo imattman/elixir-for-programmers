75df38203cf62167ad9c20f14f3c27df28729d20
Matt Phillips <matt.phillips@gmail.com>
2021-11-14 18:21:12 -0800

    restarting with second edition

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,11 @@
+defmodule Dictionary do
+  def word_list() do
+    File.read!("assets/words.txt")
+    |> String.split(~r/\n/, trim: true)
+  end
+
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+end
