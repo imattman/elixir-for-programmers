15bb90a55486b92c5e020049f833f85e5065f396
Matt Phillips <matt.phillips@gmail.com>
2021-11-15 15:00:39 -0800

    wip: read word list and store in module attribute

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,11 +1,12 @@
 defmodule Dictionary do
-  def word_list() do
-    File.read!("assets/words.txt")
-    |> String.split(~r/\n/, trim: true)
-  end
+  # build word list at compile time
+  # stored in module attribute
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split(~r/\n/, trim: true)
 
   def random_word() do
-    word_list()
+    @word_list
     |> Enum.random()
   end
 end
