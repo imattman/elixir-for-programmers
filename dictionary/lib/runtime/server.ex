4f8b5a5aada2fb35be8e5e7f22a669e6ab14ab53
Matt Phillips <matt.phillips@gmail.com>
2021-11-29 14:28:16 -0800

    wip: add simple supervision to Dictionary

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,15 +1,22 @@
 defmodule Dictionary.Runtime.Server do
   alias Dictionary.Impl.WordList
 
+  use Agent
+
   @type t :: pid()
 
   @me __MODULE__
 
-  def start_link() do
+  def start_link(_args) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.333 do
+      # periodically crash the Agent to demonstrate supervision
+      Agent.get(@me, fn _ -> exit(:kaboom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
